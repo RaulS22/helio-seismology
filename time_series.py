731d32c7935ef5d895aeded79167d1560b9d14a2
RaulS22 <rau1d3as@gmail.com>
2024-11-04 00:24:08 +0000

    new: time series

--- time_series.py
+++ time_series.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.fft as fft
-import pandas as pd
 
 """
 The time series data has two calibrations: calib1 and calib2. 
@@ -13,65 +12,58 @@ The data should be read across then down.
 
 path = 'Dossier_project_data_articles/'
 
-data1_calib1 = pd.read_csv(path+'data2_calib1_pm1_960411_961010.dat')
-data2_calib1 = pd.read_csv(path+'data2_calib1_pm2_960411_961010.dat')
-data1_calib2 = pd.read_csv(path+'data2_calib2_pm1_960411_961010.dat')
-data1_calib2 = pd.read_csv(path+'data2_calib2_pm2_960411_961010.dat')
+time_series_1_1 = np.genfromtxt(path+'data2_calib1_pm1_960411_961010.dat')
+time_series_1_2 = np.genfromtxt(path+'data2_calib1_pm2_960411_961010.dat')
+time_series_2_1 = np.genfromtxt(path+'data2_calib2_pm1_960411_961010.dat')
+time_series_2_2 = np.genfromtxt(path+'data2_calib2_pm2_960411_961010.dat')
 
-# Plotting the head of the data
-'''
-csv_files = [data1_calib1, data2_calib1, data1_calib2, data1_calib2]
-for file in csv_files:
-    print(file.head())
-    print('\n')
-'''
+#print(time_series_1_1)
+#print(time_series_1_2)
+#print(time_series_2_1)
+#print(time_series_2_2)
 
 
-# Plotting the data
+# Generate time arrays
+time_array_1_1 = np.arange(0, len(time_series_1_1))
+time_array_1_2 = np.arange(0, len(time_series_1_2))
+time_array_2_1 = np.arange(0, len(time_series_2_1))
+time_array_2_2 = np.arange(0, len(time_series_2_2))
 
-# Reshape the data to create a single continuous time series
-# Flatten the DataFrame into a 1D array to read across rows and down
 
-
-time_series_1_1 = data1_calib1.values.flatten()
-time_series_1_2 = data1_calib1.values.flatten()
-
-time_series_2_1 = data1_calib1.values.flatten()
-time_series_2_2 = data1_calib1.values.flatten()
 
 # Plot the single time series
-plt.figure(figsize=(20, 12))
+plt.figure(figsize=(18, 10))
 
 plt.subplot(2, 2, 1)
-plt.plot(time_series_1_1)
+plt.plot(time_array_1_1, time_series_1_1)
 plt.title('Time Series 1 Calib 1')
 plt.xlabel('Time')
 plt.ylabel('Value')
 plt.grid()
 
 plt.subplot(2, 2, 2)
-plt.plot(time_series_1_2)
+plt.plot(time_array_1_2, time_series_1_2)
 plt.title('Time Series 2 Calib 1')
 plt.xlabel('Time')
 plt.ylabel('Value')
 plt.grid()
 
 plt.subplot(2, 2, 3)
-plt.plot(time_series_2_1)
+plt.plot(time_array_2_1, time_series_2_1)
 plt.title('Time Series 1 Calib 2')
 plt.xlabel('Time')
 plt.ylabel('Value')
 plt.grid()
 
 plt.subplot(2, 2, 4)
-plt.plot(time_series_2_2)
+plt.plot(time_array_2_2, time_series_2_2)
 plt.title('Time Series 2 Calib 2')
 plt.xlabel('Time')
 plt.ylabel('Value')
 plt.grid()
 
 plt.tight_layout()
-plt.show()
-
+plt.savefig('Time_series.png')
+plt.close()
 
 
