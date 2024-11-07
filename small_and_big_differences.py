3a4f72b2ad2337d96324aa73a40bf02597102c4f
RaulS22 <rau1d3as@gmail.com>
2024-11-07 02:40:24 +0000

    chore: normalizing the power

--- small_and_big_differences.py
+++ small_and_big_differences.py
@@ -17,20 +17,27 @@ fft_2_2 = fft.fft(time_series_2_2)
 N = len(time_series_2_2)
 f = np.fft.fftfreq(N)
 
-#Only plotting the positive frequencies
-mask = (f > 0.03) & (f < 0.09)
-f = f[mask]
-fft_2_2 = fft_2_2[mask]
+# Normalizing by total energy (sum of squared magnitudes)
+fft_2_2_normalized_energy = np.abs(fft_2_2) / np.sqrt(np.sum(np.abs(fft_2_2)**2))
 
-# Plotting the Fourier Transform
-# We're only interested in the frequencies between 0.03 and 0.09 Hz
+# Filter positive frequencies
+positive_frequencies = f > 0
+fft_2_2_normalized_energy_positive = fft_2_2_normalized_energy[positive_frequencies]
+f_positive = f[positive_frequencies]
 
+
+# Select frequency range between 
+freq_range_mask = (f_positive >= 0.03) & (f_positive <= 0.07)
+f_selected = f_positive[freq_range_mask]
+fft_selected = fft_2_2_normalized_energy_positive[freq_range_mask]
+
+
+# Plotting the normalized Fourier Transform (with respect to energy) 
 plt.figure(figsize=(12, 6))
-plt.plot(f, np.abs(fft_2_2))
-plt.title('Fourier Transform of PM2 Calib 2')
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Amplitude')
-plt.grid()
-plt.show()
-
+plt.plot(f_selected, fft_selected)
+plt.title('Normalized Fourier Transform of PM2 Calib 2 (0.03-0.07 Hz)')
+plt.xlabel('Frequency (mHz)')
+plt.ylabel('Normalized Power')
+plt.savefig('FFT_normalized.png')
+plt.close()
 
